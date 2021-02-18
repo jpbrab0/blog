a558307b951a880978371c25e7bf27dee9518c2e
Joao Pedro <jpresende502@gmail.com>
2021-02-18 15:09:56 -0300

    Initial commit from Create Next App

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import { ThemeProvider } from "next-themes";
+
+import "@assets/main.css";
+
+import "typeface-open-sans";
+import "typeface-merriweather";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider defaultTheme="system" enableSystem={true} attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
---
+++ pages/post/[slug].js
@@ -0,0 +1,97 @@
+import Link from "next/link";
+import ReactMarkdown from "react-markdown/with-html";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import style from "react-syntax-highlighter/dist/cjs/styles/prism/dracula";
+
+import { Layout, Image, SEO, Bio } from "@components/common";
+import { getPostBySlug, getPostsSlugs } from "@utils/posts";
+
+export default function Post({ post, frontmatter, nextPost, previousPost }) {
+  return (
+    <Layout>
+      <SEO
+        title={frontmatter.title}
+        description={frontmatter.description || post.excerpt}
+      />
+
+      <article>
+        <header className="mb-8">
+          <h1 className="mb-2 text-6xl font-black leading-none font-display">
+            {frontmatter.title}
+          </h1>
+          <p className="text-sm">{frontmatter.date}</p>
+        </header>
+        <ReactMarkdown
+          className="mb-4 prose lg:prose-lg dark:prose-dark"
+          escapeHtml={false}
+          source={post.content}
+          renderers={{ code: CodeBlock, image: MarkdownImage }}
+        />
+        <hr className="mt-4" />
+        <footer>
+          <Bio className="mt-8 mb-16" />
+        </footer>
+      </article>
+
+      <nav className="flex flex-wrap justify-between mb-10">
+        {previousPost ? (
+          <Link href={"/post/[slug]"} as={`/post/${previousPost.slug}`}>
+            <a className="text-lg font-bold">
+              ← {previousPost.frontmatter.title}
+            </a>
+          </Link>
+        ) : (
+          <div />
+        )}
+        {nextPost ? (
+          <Link href={"/post/[slug]"} as={`/post/${nextPost.slug}`}>
+            <a className="text-lg font-bold">{nextPost.frontmatter.title} →</a>
+          </Link>
+        ) : (
+          <div />
+        )}
+      </nav>
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getPostsSlugs();
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const postData = getPostBySlug(slug);
+
+  if (!postData.previousPost) {
+    postData.previousPost = null;
+  }
+
+  if (!postData.nextPost) {
+    postData.nextPost = null;
+  }
+
+  return { props: postData };
+}
+
+const CodeBlock = ({ language, value }) => {
+  return (
+    <SyntaxHighlighter style={style} language={language}>
+      {value}
+    </SyntaxHighlighter>
+  );
+};
+
+const MarkdownImage = ({ alt, src }) => (
+  <Image
+    alt={alt}
+    src={require(`../../content/assets/${src}`)}
+    webpSrc={require(`../../content/assets/${src}?webp`)}
+    previewSrc={require(`../../content/assets/${src}?lqip`)}
+    className="w-full"
+  />
+);
